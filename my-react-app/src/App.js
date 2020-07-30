8bef023d697b838fe50f630ca65de85d2a017a80
punsay <punsay.prakhar@tothenew.com>
2020-07-30 23:16:48 +0530

    created a functional classless component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import './font.css';
+import Person from './Person/Person';
 
 class App extends Component {
     render(){
       return (
         <div className="App">
         <h1>Hi I am react App</h1>
+        <Person/>
         </div>
       );
     }
