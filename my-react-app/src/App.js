272852a6ac7fceedf61ea0b7d46fbe53f833b931
punsay <punsay.prakhar@tothenew.com>
2020-07-30 22:54:13 +0530

    settings changes

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './font.css';
+
+class App extends Component {
+    render(){
+      return (
+        <div className="App">
+        <h1>Hi I am react App</h1>
+        </div>
+      );
+    }
+}
+
+export default App;
