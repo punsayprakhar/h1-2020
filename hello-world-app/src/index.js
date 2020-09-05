83423bd9cface03761cfad417951a433ea39e748
punsay <punsay.prakhar@tothenew.com>
2020-09-05 21:55:46 +0530

    template string

---
+++ hello-world-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var fname = 'Punsay';
+var lname = 'Prakhar';
+
+ReactDOM.render(<h1>{`My first name is ${fname} and my last name is ${lname}`}</h1>,
+  document.getElementById('root'));
